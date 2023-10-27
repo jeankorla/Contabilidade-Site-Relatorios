1804a94db8e9ad3e84d0c413037199c0c57d299e
JeanKorla <jeansilvakorla@hotmail.com>
2023-10-27 11:40:59 -0300

    corrigindo controller

---
+++ contabilidade/app/Controllers/proposta.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class proposta extends Controller
+{
+    public function index()
+    {
+        return redirect()->to('https://www.canva.com/design/DAFtsKM2q28/hYxiNs3C8yFMELTfx9NUjw/view?utm_content=DAFtsKM2q28&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink');
+    }
+}
