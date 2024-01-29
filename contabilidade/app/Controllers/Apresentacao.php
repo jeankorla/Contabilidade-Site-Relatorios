23a051aa5ba499fc0d8edf11a5e060bd9eb854af
JeanKorla <jeansilvakorla@hotmail.com>
2024-01-29 13:03:51 -0300

    atualização no link de apresentação

--- contabilidade/app/Controllers/Apresentacao.php
+++ contabilidade/app/Controllers/Apresentacao.php
@@ -6,6 +6,6 @@ class Apresentacao extends Controller
 {
     public function index()
     {
-        return redirect()->to('https://www.canva.com/design/DAFURTy6VRw/u7hGq6KqoEwF3Js15R52uA/view?utm_content=DAFURTy6VRw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink');
+        return redirect()->to('https://www.canva.com/design/DAF6uoAOqXs/RYEeaSZH5Jq7w0IU6JU1ag/view?utm_content=DAF6uoAOqXs&utm_campaign=designshare&utm_medium=link&utm_source=editor');
     }
 }
