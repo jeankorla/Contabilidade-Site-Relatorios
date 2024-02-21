c887386ef6d54888b3a12de6245bba97c6f42954
JeanKorla <jeansilvakorla@hotmail.com>
2024-02-21 15:27:46 -0300

    mascara cnpj v3

--- contabilidade/app/Views/trocar.php
+++ contabilidade/app/Views/trocar.php
@@ -325,7 +325,7 @@ form .input-group button{
 
                 <div class="input-group">
                     <label for="cnpj">CNPJ</label>
-                    <input type="text" id="cnpj" name="cnpj" placeholder="Digite seu CNPJ" required oninput="aplicarMascaraCNPJ(this)" maxlength="14">
+                    <input type="text" id="cnpj" name="cnpj" placeholder="Digite seu CNPJ" required oninput="aplicarMascaraCNPJ(this)" maxlength="18">
                 </div>
 
                 <div class="input-group">
