623ba99f5a5a184a280ca97a12ac5f3773087cb2
JeanKorla <jeansilvakorla@hotmail.com>
2024-04-19 14:40:44 -0300

    Atualização de informações TROCA E ABERTURA

--- contabilidade/app/Controllers/AdminController.php
+++ contabilidade/app/Controllers/AdminController.php
@@ -47,25 +47,34 @@ class AdminController extends BaseController
 
     public function atualizarTrocar($id = null)
     {
-        $trocarModel = new Trocar();
+        $nome = $this->request->getPost('nome');
+        $destinatarioEmail = $this->request->getPost('email');
+        $tel = $this->request->getPost('tel');
+        $cnpj = $this->request->getPost('cnpj');
+        $nome_empresa = $this->request->getPost('nome_empresa');
+        $faturamento = $this->request->getPost('faturamento');
+        $funcionarios = $this->request->getPost('funcionarios');
+        $tributacao = $this->request->getPost('tributacao');
+        $nfe = $this->request->getPost('nfe');
+        $lancamento = $this->request->getPost('lancamento');
+        $estado = $this->request->getPost('estado');
 
-        // Atualize o registro com os novos dados.
+        $trocarModel = new Trocar;
+
         $data = [
-            $nome = $this->request->getPost('nome'),
-            $email = $this->request->getPost('email'),
-            $tel = $this->request->getPost('tel'),
-            $cnpj = $this->request->getPost('cnpj'),
-            $nome_empresa = $this->request->getPost('nome_empresa'),
-            $faturamento = $this->request->getPost('faturamento'),
-            $funcionarios = $this->request->getPost('funcionarios'),
-            $nfe = $this->request->getPost('nfe'),
-            $lancamento = $this->request->getPost('lancamento'),
-            $tributacao = $this->request->getPost('tributacao'),
-            $estado = $this->request->getPost('estado'),
+            'nome' => $nome,
+            'email' => $destinatarioEmail,
+            'tel' => $tel,
+            'cnpj' => $cnpj,
+            'nome_empresa' => $nome_empresa,
+            'faturamento' => $faturamento,
+            'funcionarios' => $funcionarios,
+            'tributacao' => $tributacao,
+            'nfe' => $nfe,
+            'lancamento' => $lancamento,
+            'estado' => $estado
         ];
 
-        // Atualize o registro com os novos dados.
-        $data = $this->request->getPost();
         $trocarModel->update($id, $data);
 
         // Redirecione de volta com uma mensagem de sucesso.
@@ -99,9 +108,6 @@ public function atualizarAbrir($id = null)
         $estado = $this->request-> getPost('estado');
 
         $abrirModel = new Abrir;
-
-        $nfe = $this->request->getPost('nfe');
-        $lancamento = $this->request->getPost('lancamento');
 
         $data = [
             'nome' => $nome,
