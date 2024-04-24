c60e8170f22f374abd3ed02ee162be3ce2428503
JeanKorla <jeansilvakorla@hotmail.com>
2024-04-24 18:08:06 -0300

    .

--- contabilidade/app/Controllers/TrocarController.php
+++ contabilidade/app/Controllers/TrocarController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Trocar;
-use App\Controllers\Services;
+use Config\Services;
 
 class TrocarController extends BaseController
 {
