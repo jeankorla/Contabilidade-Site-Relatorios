fecab3e22a842575e0988d43c086541e58925af7
JeanKorla <jeansilvakorla@hotmail.com>
2024-04-25 15:48:04 -0300

    agora vaiiiii

--- contabilidade/app/Views/contato.php
+++ contabilidade/app/Views/contato.php
@@ -208,6 +208,7 @@
       </div>
       <div class="modal-body">
         <form id="responseForm">
+          <input type="hidden" id="contactId" name="contactId">
             <div class="mb-3">
                 <label for="recipientEmail" class="col-form-label">Para:</label>
                 <input type="email" class="form-control" id="recipientEmail" name="recipientEmail" readonly>
@@ -229,8 +230,9 @@
 
 
 <script>
-function openResponseModal(email, name) {
+function openResponseModal(email, name, id) {
     document.getElementById('recipientEmail').value = email;
+    document.getElementById('contactId').value = id;  // Adicione um campo oculto para armazenar o ID
     document.getElementById('message-text').value = ''; // Limpa a mensagem anterior
     $('#responseModal').modal('show');
 }
@@ -238,18 +240,22 @@ function openResponseModal(email, name) {
 function sendResponse() {
     var email = document.getElementById('recipientEmail').value;
     var message = document.getElementById('message-text').value;
-    // Certifique-se de que o URL está correto e de que a resposta esperada é JSON
-    $.post('<?= base_url("ContatoController/sendResponse") ?>', { email: email, message: message }, function(response) {
+    var contactId = document.getElementById('contactId').value; // Capta o ID do contato
+
+    // Envia os dados, incluindo o ID do contato
+    $.post('<?= base_url("ContatoController/sendResponse") ?>', {
+        email: email,
+        message: message,
+        contactId: contactId  // Adicione isto
+    }, function(response) {
         $('#responseModal').modal('hide');
         if (response.status === 'success') {
-            // Recarrega a página ou mostra mensagem de sucesso de forma dinâmica
-            alert('Email enviado com sucesso!'); // Altere para um sistema de notificação mais robusto se necessário
-            window.location.reload(); // Opcional: remover para não recarregar a página
+            alert('Email enviado com sucesso!');
+            window.location.reload();
         } else {
-            alert('Falha ao enviar o email: ' + response.message); // Mostra a mensagem de erro do servidor
+            alert('Falha ao enviar o email: ' + response.message);
         }
     }, 'json').fail(function(xhr, status, error) {
-        // Tratamento de falha na requisição AJAX
         alert('Erro ao enviar resposta: ' + xhr.responseText);
     });
 }
