5102a4e8021570f35e6789618b7ae1f2a3d42209
JeanKorla <jeansilvakorla@hotmail.com>
2024-04-25 14:37:34 -0300

    teste jquery

--- contabilidade/app/Views/contato.php
+++ contabilidade/app/Views/contato.php
@@ -8,6 +8,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 
 <style>
     .jp1 {
