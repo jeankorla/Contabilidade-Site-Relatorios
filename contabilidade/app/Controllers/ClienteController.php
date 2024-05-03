bff3bebc5fb3c0c867bed7fc27b38304cac85727
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-03 17:15:49 -0300

    teste

--- contabilidade/app/Controllers/ClienteController.php
+++ contabilidade/app/Controllers/ClienteController.php
@@ -75,6 +75,54 @@ class ClienteController extends BaseController
          return view('editarCliente', ['registro' => $registro]);
     }
 
+    public function atualizarCliente($id = null)
+    {
+        // Coletar todos os dados do formulário
+        $data = [
+            'motivo_contato'           => $this->request->getPost('motivo_contato'),
+            'nome_contato'             => $this->request->getPost('nome_contato'),
+            'email_contato'            => $this->request->getPost('email_contato'),
+            'tel_contato'              => $this->request->getPost('tel_contato'),
+            'cpf_contato'              => $this->request->getPost('cpf_contato'),
+            'cnpj'                     => $this->request->getPost('cnpj'),
+            'nome_empresa'             => $this->request->getPost('nome_empresa'),
+            'faturamento'              => $this->request->getPost('faturamento'),
+            'funcionarios'             => $this->request->getPost('funcionarios'),
+            'tributacao'               => $this->request->getPost('tributacao'),
+            'nfe'                      => $this->request->getPost('nfe'),
+            'lancamento'               => $this->request->getPost('lancamento'),
+            'endereco_empresa_estado'  => $this->request->getPost('endereco_empresa_estado'),
+            'endereco_empresa_cep'     => $this->request->getPost('endereco_empresa_cep'),
+            'endereco_empresa_rua'     => $this->request->getPost('endereco_empresa_rua'),
+            'endereco_empresa_numero'  => $this->request->getPost('endereco_empresa_numero'),
+            'endereco_empresa_bairro'  => $this->request->getPost('endereco_empresa_bairro'),
+            'endereco_empresa_cidade'  => $this->request->getPost('endereco_empresa_cidade'),
+            'socio_nome'               => $this->request->getPost('socio_nome'),
+            'socio_nacional'           => $this->request->getPost('socio_nacional'),
+            'socio_idade'              => $this->request->getPost('socio_idade'),
+            'socio_rg'                 => $this->request->getPost('socio_rg'),
+            'socio_cpf'                => $this->request->getPost('socio_cpf'),
+            'socio_endereco_cep'       => $this->request->getPost('socio_endereco_cep'),
+            'socio_endereco_estado'    => $this->request->getPost('socio_endereco_estado'),
+            'socio_endereco_cidade'    => $this->request->getPost('socio_endereco_cidade'),
+            'socio_endereco_bairro'    => $this->request->getPost('socio_endereco_bairro'),
+            'socio_endereco_rua'       => $this->request->getPost('socio_endereco_rua'),
+            'socio_endereco_numero'    => $this->request->getPost('socio_endereco_numero'),
+            'honorario'                => $this->request->getPost('honorario'),
+            'honorario_texto'          => $this->request->getPost('honorario_texto'),
+            'inicio_contabilidade'     => $this->request->getPost('inicio_contabilidade'),
+            'competencia'              => $this->request->getPost('competencia')
+        ];
+
+        // Instanciando o modelo e inserindo os dados
+        $clienteModel = new Cliente;
+
+        $trocarModel->update($id, $data);
+
+        // Redirecione de volta com uma mensagem de sucesso.
+        return redirect()->to('AdminController')->with('success', 'Registro atualizado com sucesso.');
+    }
+
     public function excluirCliente($id = null)
     {
         $clienterModel = new Cliente();
