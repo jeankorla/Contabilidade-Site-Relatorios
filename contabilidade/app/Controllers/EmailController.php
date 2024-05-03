a554371f8b5567e19af417c54002470816245a01
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-03 17:39:30 -0300

    teste remoção de request

--- contabilidade/app/Controllers/EmailController.php
+++ contabilidade/app/Controllers/EmailController.php
@@ -318,45 +318,40 @@ public function emailDiretoria($data)
 }
 
 public function contatoEmailDiretoria($data)
-    {
-        $data = $this->request->getPost();
+{
+    $email = \Config\Services::email();
 
-        $email = \Config\Services::email();
+    $email->setFrom('controladoria@sccontab.com.br', 'Spolaor Contabilidade');
+    $email->setTo('jean@sccontab.com.br');
 
-        $email->setFrom('controladoria@sccontab.com.br', 'Spolaor Contabilidade');
-        $email->setTo('jean@sccontab.com.br');
+    $email->setSubject('Contato do site');
+    $email->setMailType('html');
 
-        $email->setSubject('Contato do site');
+    $htmlContent = "
+    <html>
+    <head>
+        <title>Contato via Site</title>
+    </head>
+    <body>
+        <h1>Dados do Contato</h1>
+        <p>Recebemos uma nova mensagem através do formulário de contato do site:</p>
+        <ul>
+            <li>Nome: {$data['name']}</li>
+            <li>Email: {$data['email']}</li>
+            <li>Mensagem: {$data['textarea']}</li>
+        </ul>
+        <p>Atenciosamente,</p>
+        <p>Equipe de Contabilidade</p>
+    </body>
+    </html>";
 
-        $email->setMailType('html');
+    $email->setMessage($htmlContent);
 
-        $htmlContent = "
-        <html>
-        <head>
-            <title>Contato via Site</title>
-        </head>
-        <body>
-            <h1>Dados do Contato</h1>
-            <p>Recebemos uma nova mensagem através do formulário de contato do site:</p>
-            <ul>
-                <li>Nome: {$data['name']}</li>
-                <li>Email: {$data['email']}</li>
-                <li>Mensagem: {$data['textarea']}</li>
-            </ul>
-            <p>Atenciosamente,</p>
-            <p>Equipe de Contabilidade</p>
-        </body>
-        </html>";
-
-        $email->setMessage($htmlContent);
-
-          if ($email->send()) {
-        echo "E-mail enviado com sucesso!";
-        } else {
-            $data = $email->printDebugger(['headers', 'subject', 'body']);
-            print_r($data); // Mostra informações sobre possíveis erros
-        }
+    if (!$email->send()) {
+        $debugData = $email->printDebugger(['headers', 'subject', 'body']);
+        log_message('error', "Falha ao enviar email de notificação para a diretoria: " . print_r($debugData, true));
     }
+}
 
     public function contatoRespostaCliente()
     {
