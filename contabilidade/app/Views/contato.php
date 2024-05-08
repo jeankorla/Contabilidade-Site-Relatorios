46f8dd76d150149410ebca424a904725291a38a0
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-08 17:04:02 -0300

    .

--- contabilidade/app/Views/contato.php
+++ contabilidade/app/Views/contato.php
@@ -235,9 +235,9 @@ button:active {
               <div class="svg-wrapper">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
+                  viewBox="0 0 20 20"
+                  width="20"
+                  height="20"
                 >
                   <path fill="none" d="M0 0h24v24H0z"></path>
                   <path
