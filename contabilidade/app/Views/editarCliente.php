e592b31a26aaec92bb9d016b7564d573719d6951
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-08 17:31:53 -0300

    button com javascript

--- contabilidade/app/Views/editarCliente.php
+++ contabilidade/app/Views/editarCliente.php
@@ -403,7 +403,7 @@
             </div>
             
             <div class="col-md-6 d-flex justify-content-end">
-                <a href="<?= base_url('PropostaController/gerarProposta/' . $data['cliente']['id']) ?>" class="btn flex items-center bg-blue-500 text-white gap-1 px-4 py-2 cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3">
+                <button onclick="location.href='<?= base_url('PropostaController/gerarProposta/' . $data['cliente']['id']) ?>'" class="flex items-center bg-blue-500 text-white gap-1 px-4 py-2 cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3">
                         Proposta
                         <svg
                             class="w-5 h-5"
@@ -419,7 +419,7 @@
                             stroke-linecap="round"
                             ></path>
                         </svg>
-                        </a>
+                        </button>
             </div>
         </form>
     </div>
