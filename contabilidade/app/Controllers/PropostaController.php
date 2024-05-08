c19911fed803929fbdf70b82d3a33277c437c860
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-08 11:05:24 -0300

    teste

--- contabilidade/app/Controllers/PropostaController.php
+++ contabilidade/app/Controllers/PropostaController.php
@@ -97,6 +97,10 @@ class PropostaController extends Controller
   <meta property="og:image:alt" content="Imagem ilustrativa" />
   
   <title>Página Inicial</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- Option 1: Include in HTML -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="assetsClientes/web/assets/mobirise-icons2/mobirise2.css">
   <link rel="stylesheet" href="assetsClientes/web/assets/mobirise-icons/mobirise-icons.css">
   <link rel="stylesheet" href="assetsClientes/bootstrap/css/bootstrap.min.css">
