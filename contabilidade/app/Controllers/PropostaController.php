e5045a5755437db5ce598f87dce481327ab5e6d3
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-17 10:58:44 -0300

    teste

--- contabilidade/app/Controllers/PropostaController.php
+++ contabilidade/app/Controllers/PropostaController.php
@@ -756,7 +756,7 @@ class PropostaController extends Controller
         <h5 class="modal-title" id="confirmacaoModalLabel">Confirmação</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body" style="padding: 0.5rem !important;">
+      <div class="modal-body" style="heigth: 0.5rem !important;">
         <p>Você tem certeza que quer enviar essa proposta?</p>
       </div>
       <div class="modal-footer">
