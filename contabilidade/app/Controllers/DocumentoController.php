34abff726777ac487f65cf7afc3795f6bb757958
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-17 13:48:42 -0300

    teste

--- contabilidade/app/Controllers/DocumentoController.php
+++ contabilidade/app/Controllers/DocumentoController.php
@@ -274,41 +274,171 @@ $variables = [
 
         $htmlContent = '
         <!DOCTYPE html>
-    <html>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <head>
-        <title>Parabéns</title>
-    </head>
-    <body>
-    <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark mb-4" style="background-color: #024A7F; z-index: 1;">
-    <div class="container-fluid navbar-container">
-        <a class="navbar-brand" href="sccontab.com.br">
-            <img class="img-fluid" src="<?php echo base_url(\'img/logo-nav.svg\') ?>" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-               
-            </ul>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.9.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/design-sem-nome-10.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
 
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="menu menu2 cid-sFCw1qGFAI" once="menu" id="menu2-23">
+    
+    <nav class="navbar navbar-dropdown navbar-expand-lg">
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="">
+                        <img src="assets/images/design-sem-nome-10.png" style="height: 3rem;">
+                    </a>
+                </span>
+                
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-white display-4" href="#top"><span class="mobi-mbri mobi-mbri-home mbr-iconfont mbr-iconfont-btn"></span></a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="index.html#features1-1i">Abertura de empresa</a></li>
+                    <li class="nav-item"><a class="nav-link link text-white display-4" href="index.html#header14-1j">Trocar de contador</a></li>
+                    <li class="nav-item"><a class="nav-link link text-white display-4" href="index.html#content4-1q">Baixe nosso App</a>
+                    </li></ul>
+                
+                <div class="navbar-buttons mbr-section-btn"><a class="btn btn-warning-outline display-4" href="https://api.whatsapp.com/send?phone=551333614324&text=Ol%C3%A1,%20vim%20do%20site%20sccontab.com.br%20e%20tenho%20d%C3%BAvidas%20sobre%20algum%20assunto!" target="_blank">Dúvidas</a></div>
+            </div>
+        </div>
+    </nav>
+</section>
+
+<section data-bs-version="5.1" class="header14 cid-sFzxmVl7J6" id="header14-1f">
+
+    
+
+    
+
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 col-md-6 image-wrapper">
+                <img src="assets/images/contabilidade.png" alt="">
+            </div>
+            <div class="col-12 col-md">
+                <div class="text-wrapper">
+                    <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2"><strong>Prezado(a) ' . $socioNome . '</strong></h1>
+                    <p class="mbr-text mbr-fonts-style display-7">Nós, da<strong>Spolaor Company</strong>, não podemos deixar de agradecer por essa nova parceria de sucesso que estamos firmando! Com certeza, trata-se de uma nova oportunidade de sucesso para ambas as partes e que colheremos muitos frutos dela.
+Esperamos que seja somente o início de uma longa jornada juntos e saibam que podem contar conosco sempre!
+<br>
+Você irá receber um e-mail em ' . $socioEmail . ' com seu contrato!
+<br>Grande abraço,</p>    
+                </div>
+            </div>
         </div>
     </div>
-</nav>
+</section>
 
+<section data-bs-version="5.1" class="info3 cid-tT8UmQ9Xzs" id="info3-2">
 
-        <h1>Parabéns, $socioNome!</h1>
-        <h3>Estamos muito felizes por você escolher nossos serviços. Esperamos atender sempre suas expectativas!</h3>
-        <p>Um e-mail com seu contrato foi enviado para ' . $socioEmail . '</p>
-
-
-
-    </body>
-    </html>
     
+
+    
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(0, 0, 0);">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style align-center mb-4 display-1"><strong>A Contabilidade pode ajudar</strong></h4>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-7">
+                            Entender a necessidade da sua empresa, encontrar soluções inovadoras e compatíveis com o mercado, associados à melhor solução técnica, faz parte da essência da Spolaor Contabilidade, uma empresa diferenciada e moderna, pois sempre busca a satisfação do cliente na sua expectativa máxima.</p>
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-warning-outline display-4" href="">Entrar em contato</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="map2 cid-sFAxp7Y5iM" id="map2-1y">
+    
+    
+    <div>
+        
+        <div class="google-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.4965324667614!2d-46.31189452582563!3d-23.97823727703765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce11ab76a86761%3A0x3d8b4329ea300dc3!2sSpolaor%20Contabilidade!5e0!3m2!1spt-BR!2sbr!4v1697727091039!5m2!1spt-BR!2sbr" allowfullscreen=""></iframe></div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-sFCygHrmNf" once="footers" id="footer3-24">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row align-center mbr-white">
+            
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="https://www.facebook.com/spolaorcontabilidade/?paipv=0&eav=AfZgEast7gbz-AXXb65BoXmkE4Zm5qg-N_8k_BbPJ_4-nM1TF-0gjeER-aScEh12YVE&_rdr" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.instagram.com/sccontab/" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://api.whatsapp.com/send?phone=551333614324&text=Ol%C3%A1,%20vim%20do%20site%20sccontab.com.br%20e%20tenho%20d%C3%BAvidas%20sobre%20algum%20assunto!" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.linkedin.com/company/spolaor-contabilidade/mycompany/" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-linkedin socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Copyright 2025 SpolaorCompany. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+</section><section class="display-7" style="padding: 0;align-items: center;justify-content: center;flex-wrap: wrap;    align-content: center;display: flex;position: relative;height: 4rem;"><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
     ';
 
         file_put_contents($filePath, $htmlContent);
