3b650135c0ab90e87210f7f2eeb6191d318e23c3
JeanKorla <jeansilvakorla@hotmail.com>
2024-05-17 13:22:36 -0300

    teste

--- contabilidade/app/Controllers/DocumentoController.php
+++ contabilidade/app/Controllers/DocumentoController.php
@@ -272,16 +272,44 @@ $variables = [
             mkdir('parabens', 0755, true);
         }
 
-        $htmlContent = "<!DOCTYPE html>
+        $htmlContent = '
+        <!DOCTYPE html>
     <html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <head>
         <title>Parabéns</title>
     </head>
     <body>
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark mb-4" style="background-color: #024A7F; z-index: 1;">
+    <div class="container-fluid navbar-container">
+        <a class="navbar-brand" href="sccontab.com.br">
+            <img class="img-fluid" src="<?php echo base_url(\'img/logo-nav.svg\') ?>" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+               
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+
         <h1>Parabéns, $socioNome!</h1>
-        <p>Estamos muito felizes por você escolher nossos serviços. Esperamos atender sempre suas expectativas!</p>
+        <h3>Estamos muito felizes por você escolher nossos serviços. Esperamos atender sempre suas expectativas!</h3>
+        <p>Um e-mail com seu contrato foi enviado para ' . $socioEmail . '</p>
+
+
+
     </body>
-    </html>";
+    </html>
+    
+    ';
 
         file_put_contents($filePath, $htmlContent);
 
