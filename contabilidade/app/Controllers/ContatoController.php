e67ebd5cf882b2feadc094ab525bffa6fb4edbcb
TestUserMarcos <jeankorla15@gmail.com>
2024-07-05 10:49:33 -0300

    blacklist email

--- contabilidade/app/Controllers/ContatoController.php
+++ contabilidade/app/Controllers/ContatoController.php
@@ -34,38 +34,54 @@ class ContatoController extends BaseController
 
     public function store() 
     {
-    // Verificação de proteção antispam
-    if (isset($_POST["website"]) && $_POST["website"] !== "") {
-        // Se o campo não está vazio, é provável que seja um bot de spam
-        http_response_code(400);
-        exit;
-    }
+        // Verificação de proteção antispam
+        if (isset($_POST["website"]) && $_POST["website"] !== "") {
+            // Se o campo não está vazio, é provável que seja um bot de spam
+            http_response_code(400);
+            exit;
+        }
 
         $name = $this->request->getPost('name');
-    $email = $this->request->getPost('email');
-    $textarea = $this->request->getPost('textarea');
+        $email = $this->request->getPost('email');
+        $textarea = $this->request->getPost('textarea');
 
-    // Verifica se todos os campos estão vazios
-    if (empty($name) && empty($email) && empty($textarea)) {
-        // Se todos os campos estiverem vazios, não faça nada ou retorne uma mensagem de erro
-        return redirect()->back()->with('error', 'O formulário está vazio.')->withInput();
-    } else {
-        // Se algum campo estiver preenchido, continue com o processo
-        $Contato = new Contato;
-
-        $data = [
-            'name' => $name,
-            'email' => $email,
-            'textarea' => $textarea,
+        // Lista de e-mails na blacklist
+        $blacklist = [
+            'ericjonesmyemail@gmail.com',
+            'hudspeth.roberta@outlook.com',
+            'lamontagne.drusilla@gmail.com',
+            'noah.eade@gmail.com',
+            'correa.monica@msn.com',
+            'kayleighbpsteamship@gmail.com',
+            'withrow.john@gmail.com',
+            'whitfeld.kristen@gmail.com',
         ];
 
-        $Contato->insert($data);
+        // Verifica se todos os campos estão vazios
+        if (empty($name) && empty($email) && empty($textarea)) {
+            // Se todos os campos estiverem vazios, não faça nada ou retorne uma mensagem de erro
+            return redirect()->back()->with('error', 'O formulário está vazio.')->withInput();
+        } elseif (in_array($email, $blacklist)) {
+            // Se o e-mail estiver na blacklist, retorne uma mensagem de erro
+            return redirect()->back()->with('error', 'Este e-mail está bloqueado.')->withInput();
+        } else {
+            // Se algum campo estiver preenchido e o e-mail não estiver na blacklist, continue com o processo
+            $Contato = new Contato;
 
-        $this->emailController->contatoEmailDiretoria($data);
+            $data = [
+                'name' => $name,
+                'email' => $email,
+                'textarea' => $textarea,
+            ];
 
-        return redirect()->back()->with('success', 'Formulário enviado com sucesso.')->withInput();
+            $Contato->insert($data);
+
+            $this->emailController->contatoEmailDiretoria($data);
+
+            return redirect()->back()->with('success', 'Formulário enviado com sucesso.')->withInput();
+        }
     }
-}
+
 
     public function excluirContato($id = null)
     {
