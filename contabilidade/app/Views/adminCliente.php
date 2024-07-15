cc7c39628fae8995e37370d3adf469f01d0b8345
JeanKorla <jeansilvakorla@hotmail.com>
2024-07-15 08:30:53 -0300

    teste

--- contabilidade/app/Views/adminCliente.php
+++ contabilidade/app/Views/adminCliente.php
@@ -183,11 +183,10 @@
           </thead>
           <tbody>
          <?php foreach ($data as $item) : ?>
-    <tr>
-        <td>
+             <tr>
+              <td>
             <div style="display: flex; gap: 10px">
-                <a href="<?php echo base_url('ClienteController/arquivarCliente/' . ($item['empresa']['id'] ?? '')) ?>" class="btn btn-danger"><i class="bi bi-trash-fill"></i></a>
-                <a href="<?php echo base_url('ClienteController/editarCliente/' . ($item['cliente']['id'] ?? '')) ?>" class="btn btn-warning"><i class="bi bi-pencil-square"></i></a>
+            <a href="<?php echo base_url('DocumentsController/showDocuments/' . $item['cliente']['id']) ?>" class="btn btn-warning"><i class="bi bi-eye-fill"></i></i></a>
             </div>
         </td>
          
