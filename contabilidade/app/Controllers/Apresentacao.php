ad0815f245d455781a251de6cb28cf85d062b32d
JeanKorla <jeansilvakorla@hotmail.com>
2024-08-15 21:54:20 -0300

    teste

--- contabilidade/app/Controllers/Apresentacao.php
+++ contabilidade/app/Controllers/Apresentacao.php
@@ -4,7 +4,6 @@ use CodeIgniter\Controller;
 
 class Apresentacao extends Controller
 {
-    //
     public function index()
     {
         return redirect()->to('https://www.canva.com/design/DAF6uoAOqXs/RYEeaSZH5Jq7w0IU6JU1ag/view?utm_content=DAF6uoAOqXs&utm_campaign=designshare&utm_medium=link&utm_source=editor');
