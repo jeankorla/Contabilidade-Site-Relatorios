1c775474de012ce59b0331d277cef708402247ab
JeanKorla <jeansilvakorla@hotmail.com>
2024-08-15 22:30:11 -0300

    teste

--- contabilidade/app/Controllers/ClienteController.php
+++ contabilidade/app/Controllers/ClienteController.php
@@ -96,7 +96,7 @@ class ClienteController extends BaseController
 
         // Geração do token único para o cliente
         $token = bin2hex(random_bytes(16));
-        
+
         // Coletar todos os dados do formulário para o cliente
         $data = [
             'motivo' => $this->request->getPost('motivo'),
@@ -117,11 +117,10 @@ class ClienteController extends BaseController
         $clienteModel = new Cliente_lead();
         $existingClient = $clienteModel->where('email', $data['email'])
                                     ->orWhere('cpf', $data['cpf'])
-                                    ->orWhere('cnpj', $data['cnpj'])
                                     ->first();
 
         if ($existingClient) {
-            return redirect()->back()->with('error', 'Erro: O e-mail, CPF ou CNPJ já está registrado.')->withInput();
+            return redirect()->back()->with('error', 'Erro: O e-mail ou CPF já está registrado.')->withInput();
         }
 
         // Coletar todos os dados do formulário para a Empresa
@@ -166,6 +165,14 @@ class ClienteController extends BaseController
                 return redirect()->back()->with('error', 'Erro ao registrar empresa: ' . implode(', ', $errors))->withInput();
             }
         }
+
+        $existingCompany = $clienteModel->where('cnpj', $data['cnpj'])
+                                    ->first();
+
+        if ($existingCompany) {
+            return redirect()->back()->with('error', 'Erro: O Cnpj já está registrado.')->withInput();
+        }
+
 
         if ($data['cnpj']) {
             // Enviar o CNPJ e clienteId para o EmpresaController
