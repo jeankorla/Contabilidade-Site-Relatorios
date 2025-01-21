d5216139f364c9fafa5ee80db8ab763b492f3b05
meth123 <mateusteixeira372@gmail.com>
2025-01-21 14:40:35 -0300

    ajustes

--- contabilidade/app/Controllers/ContatoController.php
+++ contabilidade/app/Controllers/ContatoController.php
@@ -34,98 +34,38 @@ class ContatoController extends BaseController
 
     public function store() 
     {
-        // Verificação de proteção antispam
-        if (isset($_POST["website"]) && $_POST["website"] !== "") {
-            // Se o campo não está vazio, é provável que seja um bot de spam
-            http_response_code(400);
-            exit;
-        }
+    // Verificação de proteção antispam
+    if (isset($_POST["website"]) && $_POST["website"] !== "") {
+        // Se o campo não está vazio, é provável que seja um bot de spam
+        http_response_code(400);
+        exit;
+    }
 
         $name = $this->request->getPost('name');
-        $email = $this->request->getPost('email');
-        $textarea = $this->request->getPost('textarea');
+    $email = $this->request->getPost('email');
+    $textarea = $this->request->getPost('textarea');
 
-        // Lista de nomes bloqueados
-        $blacklistNames = [
-            'Robertpainy',
-            'TESTE Mateus',
-        ];
-        
-        // Lista de e-mails na blacklist
-        $blacklistEmails = [
-            'ericjonesmyemail@gmail.com',
-            'hudspeth.roberta@outlook.com',
-            'lamontagne.drusilla@gmail.com',
-            'noah.eade@gmail.com',
-            'correa.monica@msn.com',
-            'kayleighbpsteamship@gmail.com',
-            'withrow.john@gmail.com',
-            'whitfeld.kristen@gmail.com',
-            'joannariggs278@gmail.com',
-            'abuse@registry.godaddy',
-            'Gemma@registry.godaddy',
-            'registry-help@registry.godaddy',
-            'help@registry.godaddy',
-            'iana@registry.godaddy',
-            'sales@crestinfosystems.com',
-            'distinctpius@gmail.com',
-            'francois@toituressublimes.ca',
-            'marley.home@cogeco.ca',
-            'rayeko@gmail.com',
-            'mona1@tampabay.rr.com',
-            'gbautista0@gmail.com',
-            '2428phillips@gmail.com',
-            'pfaffcd@comcast.net',
-            'Stephanie@ocajuns.com',
-            'bsilverthorn@gmail.com',
-            'Khalifamin9@gmail.com',
-            '1kimsmart@gmail.com',
-            'relativelyrisky@gmail.com',
-            'jeannie@sailhealthconcierge.com',
-            'mwenzel@csgcl.com',
-            'aitanmenash@gmail.com',
-            'vought82@yahoo.com',
-            'kcharko@comcast.net',
-            'duckman911@comcast.net',
-            'melkonians@aol.com',
-            'tlawson@ripleymetalworks.com',
-            'm.jabbar.bcs@gmail.com',
-            'ccarden@coolmanbuilt.com',
-            'scovensdiane@gmail.com',
-        ];
-
-            // Verifica se todos os campos estão vazios
-        if (empty($name) && empty($email) && empty($textarea)) {
-            // Se todos os campos estiverem vazios, não faça nada ou retorne uma mensagem de erro
-            return redirect()->back()->with('error', 'O formulário está vazio.')->withInput();
-        } 
-        // Verifica se o nome está na blacklist
-        elseif (in_array($name, $blacklistNames)) {
-            return redirect()->back()->with('error', 'Este nome está bloqueado.')->withInput();
-        } 
-        // Verifica se o e-mail está na blacklist
-        elseif (in_array($email, $blacklistEmails)) { // 
-            return redirect()->back()->with('error', 'Este e-mail está bloqueado.')->withInput();
-        } 
-        else {
-            // Se passar nas verificações, continue com o processo
-            $Contato = new Contato;
+    // Verifica se todos os campos estão vazios
+    if (empty($name) && empty($email) && empty($textarea)) {
+        // Se todos os campos estiverem vazios, não faça nada ou retorne uma mensagem de erro
+        return redirect()->back()->with('error', 'O formulário está vazio.')->withInput();
+    } else {
+        // Se algum campo estiver preenchido, continue com o processo
+        $Contato = new Contato;
 
         $data = [
             'name' => $name,
             'email' => $email,
             'textarea' => $textarea,
         ];
-       
-            $Contato->insert($data);
 
-            $this->emailController->contatoEmailDiretoria($data);
-            $this->emailController->contatoEmailCliente($data);
+        $Contato->insert($data);
 
-            return redirect()->back()->with('success', 'Formulário enviado com sucesso.')->withInput();
-        }
+        $this->emailController->contatoEmailDiretoria($data);
+
+        return redirect()->back()->with('success', 'Formulário enviado com sucesso.')->withInput();
     }
-
+}
 
     public function excluirContato($id = null)
     {
